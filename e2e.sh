043e2897d57bebfa9ca2b90e505dbeb94dc5c2bf
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-11-16 14:47:26 +0900

    run twice

--- e2e.sh
+++ e2e.sh
@@ -6,3 +6,4 @@ make build
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 helm template nginx-stable/nginx-ingress | ./rendered
+helm template nginx-stable/nginx-ingress | ./rendered
