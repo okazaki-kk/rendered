4cf8755b4302a9ebf4e32ea864a5ed731d69f58e
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-11-16 14:42:02 +0900

    add e2e

---
+++ e2e.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+make build
+
+helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo update
+helm template nginx-stable/nginx-ingress | ./rendered
